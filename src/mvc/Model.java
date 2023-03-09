352ef6ad8cc0f600323d0a3b3cc15086f7333329
JoaoLucasVeras <joao.lucas.veras9@gmail.com>
2023-03-09 15:40:43 -0800

    Added Java files into minefield and also included given java files for mvc package

--- src/mvc/Model.java
+++ src/mvc/Model.java
@@ -2,4 +2,24 @@ package mvc;
 
 public class Model {
 
+	public boolean getUnsavedChanges() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void setFileName(String fName) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setUnsavedChanges(boolean b) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public String getFileName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }

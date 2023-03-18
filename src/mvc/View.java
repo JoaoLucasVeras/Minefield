4ee34fe449288bcb3632dd134a5791b6984a870d
williamtran955 <william.g.tran@sjsu.edu>
2023-03-17 21:32:42 -0700

    added View methods

--- src/mvc/View.java
+++ src/mvc/View.java
@@ -1,13 +1,33 @@
 package mvc;
 
-public class View {
+import tools.*;
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+import java.awt.event.*;
+import java.beans.*;
+import java.util.*;
 
-	public void setModel(Model model) {
-		// TODO Auto-generated method stub
-		
+public class View extends JPanel implements PropertyChangeListener {
+	private Model model;
+
+	public View(Model newModel) {
+		model = newModel;
+		setSize(500,500);
+		Border blackline = BorderFactory.createLineBorder(Color.black);
+		setBorder(blackline);
+		setBackground(Color.LIGHT_GRAY);
+	}
+	public void setModel(Model newModel) {
+		this.model.removePropertyChangeListener(this);
+		this.model = newModel;
+		this.model.initSupport(); // defined in Bean
+		this.model.addPropertyChangeListener(this);
+		repaint();
 	}
 
-	
-	
-	
+	public propertyChange(PropertyChangeEvent evt){
+		repaint();
+	}
+
 }
